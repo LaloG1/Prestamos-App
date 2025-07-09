a1ff8b8ab09fa1cd743a14020d9db2c1bab4d03f
EduardoGarciaMendoza <edugarmend@gmail.com>
2025-07-09 15:06:23 -0600

    Base de datos corregida

---
+++ db/prestamos.ts
@@ -0,0 +1,46 @@
+// db/vacaciones.ts
+import { SQLiteDatabase } from "expo-sqlite";
+
+export async function openVacacionesDb(db: SQLiteDatabase) {
+  await db.execAsync("PRAGMA foreign_keys = ON;");
+
+  await db.execAsync(`
+          CREATE TABLE IF NOT EXISTS clientes (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            nombre TEXT NOT NULL,
+            telefono TEXT,
+            notas TEXT,
+            created_at TEXT,
+            updated_at TEXT
+          
+        );
+
+          CREATE TABLE IF NOT EXISTS prestamos (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            cliente_id INTEGER NOT NULL,
+            monto REAL NOT NULL,
+            monto_original REAL NOT NULL,
+            interes REAL NOT NULL,
+            estado TEXT NOT NULL DEFAULT 'pendiente',
+            notas TEXT,
+            created_at TEXT,
+            updated_at TEXT,
+            FOREIGN KEY (cliente_id) REFERENCES clientes(id) ON DELETE CASCADE
+          
+        );
+
+        
+          CREATE TABLE IF NOT EXISTS pagos (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            prestamo_id INTEGER NOT NULL,
+            cliente_id INTEGER NOT NULL,
+            monto REAL NOT NULL,
+            tipo_pago TEXT NOT NULL,
+            fecha_pago TEXT NOT NULL,
+            created_at TEXT,
+            updated_at TEXT,
+            FOREIGN KEY (cliente_id) REFERENCES clientes(id) ON DELETE CASCADE,
+            FOREIGN KEY (prestamo_id) REFERENCES prestamos(id) ON DELETE CASCADE
+           );
+  `);
+}
